14bc22b963a8a10542da9acd7c0fd498905e1d6c
Aadhu4324 <adarshahamed70@gmail.com>
2024-10-16 17:42:46 +0530

    --

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,12 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -30,4 +28,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
